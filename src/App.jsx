3abf1dd400c20ee68556354156456455818ae446
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-03-28 13:04:21 -0300

    add component header

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,29 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import Cards from "./components/Cards";
+import Header from "./components/Header";
 
 export default function App() {
-  const [produtos, setProdutos] = useState([]);
+const [produtos, setProdutos] = useState([]);
   
   
-  async function getData()
-  {
-    let dados = await axios.get("https://fakestoreapi.com/products/")
-    let prods = await dados.data;
-    setProdutos(prods);
-  }
-  
-  useEffect(() => {
-    // axios
-    // .get("https://fakestoreapi.com/products/")
-    // .then((response) => {
-    //   setProdutos(response.data);
-    //   console.log("dados", response.data)
-    //   console.log("produtos", produtos)
-    //   console.log("preço", produtos[0].price)
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // });
-    getData();
-  }, [])
-  
-
-  // const propsProdutos = {
-  //   titule:produtos[2],
-  // }
+  useEffect(()=>{
+    async function getData()
+    {
+      let dados = await axios.get("https://fakestoreapi.com/products?limit=6")
+      let prods = await dados.data;
+      console.log(prods)
+      setProdutos(prods);
+    }
+    getData()
+  },[])
 
   return (
     <>
-    {
-      produtos.map((p) => {
-        console.log(p.price)
-        return <Cards price={p.price}/>
-      })
-    }
-    
+    <Header/>
+    <section>
+    {/* {produtos.map((p,key) => { return <Cards price={p.price} title={p.title} img={p.image} key={key}/>})} */}
+    </section>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+  return (
+    <header>
+    <span><img src="src\assets\fire.png" alt="..." /></span>
+    <nav>
+      <ul><li><a href="#">Home</a></li></ul>
+      <ul><li><a href="#">Categorias</a></li></ul>
+      <ul><li><a href="#">Produtos</a></li></ul>
+      <ul><li><a href="#">Contato</a></li></ul>
+    </nav>
+    <label htmlFor="search">
+    <input type="search" placeholder="Busque seu produto"/>
+    <span><img src="https://cdn-icons-png.flaticon.com/512/8336/8336588.png" alt="..." /></span>
+    </label>
+    <span><img src="src/assets/carrinho-de-compras.png" alt="..." /></span>
+    
+    <label for="burger" class="burger">
+    <input id="burger" type="checkbox"/>
+    <span></span>
+    <span></span>
+    <span></span>
+    </label>
+  </header>
+  )
+}
