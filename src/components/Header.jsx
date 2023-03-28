3bcc90d2f67c9077c9698f0e847c41a7802d7407
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-03-28 14:01:12 -0300

    fix bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './header.css'
 export default function Header() {
   return (
     <header>
-    <span><img src="\assets\fire.png" alt="..." /></span>
+    <span><img src="https://cdn-icons-png.flaticon.com/512/1453/1453025.png" alt="..." /></span>
     <nav>
       <ul><li><a href="#">Home</a></li></ul>
       <ul><li><a href="#">Categorias</a></li></ul>
@@ -15,7 +15,7 @@ export default function Header() {
     <input type="search" placeholder="Busque seu produto"/>
     <span><img src="https://cdn-icons-png.flaticon.com/512/8336/8336588.png" alt="..." /></span>
     </label>
-    <span><img src="\assets\carrinho-de-compras.png" alt="..." /></span>
+    <span><img src="https://cdn-icons-png.flaticon.com/512/2543/2543193.png" alt="..." /></span>
     
     <label for="burger" class="burger">
     <input id="burger" type="checkbox"/>
